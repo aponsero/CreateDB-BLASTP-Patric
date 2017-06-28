84687f414b28b5707911409a8be9dcc645a3ff8b
aponsero <alise.ponsero@gmail.com>
2017-06-28 18:56:19 +0200

    Update run_taxo_log.sh

--- workers/run_taxo_log.sh
+++ workers/run_taxo_log.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#PBS -W group_list=bhurwitz
+#PBS -W group_list=yourlist
 #PBS -q standard
 #PBS -l select=1:ncpus=1:mem=10gb
 #PBS -l walltime=08:00:00
 #PBS -l cput=08:00:00
-#PBS -M aponsero@email.arizona.edu
+#PBS -M yourmail@email.arizona.edu
 #PBS -m bea
 
 LOG="$STDOUT_DIR/tax.log"
