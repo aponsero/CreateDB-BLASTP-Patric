17fe1f54f5e1472afd4ea7b5b9e562bea52eb816
aponsero <alise.ponsero@gmail.com>
2017-08-01 19:47:35 +0200

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,17 +4,17 @@ export BIN_DIR="/rsgrps/bhurwitz/hurwitzlab/bin"
 export CD_HIT="/rsgrps/bhurwitz/alise/tools/cdhit/cd-hit"
 # genomes configs
 export SPLIT_SIZE=300
-export GENOME_LIST="/rsgrps/bhurwitz/hurwitzlab/data/patric/genomes/test_GIT"
-export SPLIT_DIR="/rsgrps/bhurwitz/hurwitzlab/data/patric/genomes/test_download"
-export DB_DIR="/rsgrps/bhurwitz/hurwitzlab/data/patric/genomes/test_BLASTP_DB"
+export GENOME_LIST="YOUR/PATH/HERE"
+export SPLIT_DIR="YOUR/PATH/HERE"
+export DB_DIR="YOUR/PATH/HERE"
 # scripts configs
 export SCRIPT_DIR="$PWD/scripts"
 export WORKER_DIR="$SCRIPT_DIR/workers"
 export CD_SIM="0.9"
 # user info
-export MAIL_USER="aponsero@email.arizona.edu"
+export MAIL_USER="youremail@email.arizona.edu"
 export MAIL_TYPE="bea"
-export GROUP="bhurwitz"
+export GROUP="yourgroup"
 export QUEUE="windfall"
 
 
